8bf24532621814d2d0ad179a73155bc217fbf9a0
Roscochof <andonimc92@hotmail.com>
2023-02-02 12:40:27 +0100

    CPP3ex01 done

--- CPP3/ex01/main.cpp
+++ CPP3/ex01/main.cpp
@@ -1,25 +1,20 @@
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
 
 int	main(void)
 {
 	ClapTrap	player("Johnny");
-	ClapTrap	player2("Jimmy");
+	ScavTrap	player2("Jimmy");
 
 	std::cout << "LETS FIGHT!" << std::endl;
 	player.attack("Jimmy");
-	player2.takeDamage(5);
+	player2.takeDamage(0);
 	player.attack("Jimmy");
-	player2.takeDamage(5);
+	player2.takeDamage(0);
 	player2.beRepaired(10);
 	player2.attack("Johnny");
-	player.takeDamage(2);
-	player.takeDamage(2);
+	player.takeDamage(20);
 	player.beRepaired(10);
-	ClapTrap	player3(player);
-	player = player2;
-	player.takeDamage(0);
-	player2.takeDamage(0);
-	std::cout << "COPY -> ";
-	player3.takeDamage(0);
+	std::cout << "What an unfair fight..." << std::endl;
 	return (0);
 }
