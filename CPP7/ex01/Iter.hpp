b843fead0c6d64763e3c9ab6a2fa127f9fbe4c59
Andoni Marzana cuenca <amarzana@c4r1s2.42urduliz.com>
2023-02-27 13:00:05 +0100

    CPP7ex01 revised

--- CPP7/ex01/Iter.hpp
+++ CPP7/ex01/Iter.hpp
@@ -6,7 +6,7 @@
 /*   By: amarzana <amarzana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 18:52:42 by amarzana          #+#    #+#             */
-/*   Updated: 2023/02/26 19:15:18 by amarzana         ###   ########.fr       */
+/*   Updated: 2023/02/27 12:58:47 by amarzana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,27 @@
 
 # include <iostream>
 
+//An example of a function for ::iter that prints the param
 template <typename T>
-void	print(T &value)
+void	print(const T value)
 {
 	std::cout << value << " ";
 }
 
+//An example of a function that modifies the given param
 template <typename T>
-void iter(T* array, int length, void (*func)(T&))
+void add_one(T& x)
+{
+    x += 1;
+}
+
+// Function that works with arrays of multiple data types
+// Params:
+// 1. The address of an array. 
+// 2. The length of the given array. 
+// 3. A function pointer
+template <typename T>
+void iter(T* array, int length, void (*func)(T &))
 {
 	for (int i = 0; i < length; i++)
 		func(array[i]);
--- CPP7/ex01/main.cpp
+++ CPP7/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarzana <amarzana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 18:52:24 by amarzana          #+#    #+#             */
-/*   Updated: 2023/02/26 19:18:33 by amarzana         ###   ########.fr       */
+/*   Updated: 2023/02/27 12:59:28 by amarzana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,26 @@ int	main(void)
 {
 	std::cout << "===== Printing an int array =====" << std::endl;
 	int int_array[] = {1, 2, 3, 4};
-	iter(int_array, 4, print);
-	std::cout << std::endl;
+	::iter(int_array, 4, ::print);
+	std::cout << std::endl << "* Adding one to an int array *" << std::endl;
+	::iter(int_array, 4, ::add_one);
+	::iter(int_array, 4, ::print);
+	std::cout << std::endl << std::endl;
 
 	std::cout << "===== Printing a double array =====" << std::endl;
 	double double_array[] = {1.1, 2.2, 3.3, 4.4, 5.5};
-	iter(double_array, 5, print);
-	std::cout << std::endl;
+	::iter(double_array, 5, ::print);
+	std::cout << std::endl << "* Adding one to a double array *" << std::endl;
+	::iter(double_array, 5, ::add_one);
+	::iter(double_array, 5, ::print);
+	std::cout << std::endl << std::endl;
 
 	std::cout << "===== Printing a char array =====" << std::endl;
 	char char_array[] = {'a', 'b', 'c', 'd', 'e', 'f'};
-	iter(char_array, 6, print);
+	::iter(char_array, 6, ::print);
+	std::cout << std::endl << "* Adding one to a char array *" << std::endl;
+	::iter(char_array, 6, ::add_one);
+	::iter(char_array, 6, ::print);
 	std::cout << std::endl;
 
 	return (0);
