6ff9b49523043327832a23dfac1bfedfa0a04af4
Andoni Marzana cuenca <amarzana@c4r1s2.42urduliz.com>
2023-02-27 12:25:57 +0100

    CPP7ex00 revised

--- CPP7/ex00/main.cpp
+++ CPP7/ex00/main.cpp
@@ -6,55 +6,14 @@
 /*   By: amarzana <amarzana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 18:15:17 by amarzana          #+#    #+#             */
-/*   Updated: 2023/02/26 18:48:43 by amarzana         ###   ########.fr       */
+/*   Updated: 2023/02/27 12:25:28 by amarzana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include "Template.hpp"
+#include "whatever.hpp"
 
-/* int main(void)
-{
-	{
-		std::cout << "===== with int =====" << std::endl;
-		int a = 1;
-		int b = 2;
-
-		std::cout << "a = " << a << " and b = " << b << std::endl;
-		swap(a, b);
-		std::cout << "* swap! *" << std::endl;
-		std::cout << "a = " << a << " and b = " << b << std::endl;
-		std::cout << "min = " << min(a, b) << std::endl;
-		std::cout << "max = " << max(a, b) << std::endl;
-	}
-	{
-		std::cout << "===== with char =====" << std::endl;
-		char a = 'A';
-		char b = 'B';
-
-		std::cout << "a = " << a << " and b = " << b << std::endl;
-		swap(a, b);
-		std::cout << "* swap! *" << std::endl;
-		std::cout << "a = " << a << " and b = " << b << std::endl;
-		std::cout << "min = " << min(a, b) << std::endl;
-		std::cout << "max = " << max(a, b) << std::endl;
-	}
-	{
-		std::cout << "===== with double =====" << std::endl;
-		double a = 3.14;
-		double b = 1.23;
-
-		std::cout << "a = " << a << " and b = " << b << std::endl;
-		swap(a, b);
-		std::cout << "* swap! *" << std::endl;
-		std::cout << "a = " << a << " and b = " << b << std::endl;
-		std::cout << "min = " << min(a, b) << std::endl;
-		std::cout << "max = " << max(a, b) << std::endl;
-	}
-	return (0);
-} */
-
-int main( void ) {
+/* int main( void ) {
 	int a = 2;
 	int b = 3;
 	::swap( a, b );
@@ -68,4 +27,57 @@ int main( void ) {
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return 0;
+} */
+
+int main(void)
+{
+	{
+		std::cout << "===== with int =====" << std::endl;
+		int a = 1;
+		int b = 2;
+
+		std::cout << "a = " << a << " and b = " << b << std::endl;
+		::swap(a, b);
+		std::cout << "* swap! *" << std::endl;
+		std::cout << "a = " << a << " and b = " << b << std::endl;
+		std::cout << "min = " << ::min(a, b) << std::endl;
+		std::cout << "max = " << ::max(a, b) << std::endl;
+	}
+	{
+		std::cout << "===== with char =====" << std::endl;
+		char a = 'A';
+		char b = 'B';
+
+		std::cout << "a = " << a << " and b = " << b << std::endl;
+		::swap(a, b);
+		std::cout << "* swap! *" << std::endl;
+		std::cout << "a = " << a << " and b = " << b << std::endl;
+		std::cout << "min = " << ::min(a, b) << std::endl;
+		std::cout << "max = " << ::max(a, b) << std::endl;
+	}
+	{
+		std::cout << "===== with double =====" << std::endl;
+		double a = 3.14;
+		double b = 1.23;
+
+		std::cout << "a = " << a << " and b = " << b << std::endl;
+		::swap(a, b);
+		std::cout << "* swap! *" << std::endl;
+		std::cout << "a = " << a << " and b = " << b << std::endl;
+		std::cout << "min = " << ::min(a, b) << std::endl;
+		std::cout << "max = " << ::max(a, b) << std::endl;
+	}
+	{
+		std::cout << "===== with string =====" << std::endl;
+		std::string a = "prueba1";
+		std::string b = "prueba2";
+
+		std::cout << "a = " << a << " and b = " << b << std::endl;
+		::swap(a, b);
+		std::cout << "* swap! *" << std::endl;
+		std::cout << "a = " << a << " and b = " << b << std::endl;
+		std::cout << "min = " << ::min(a, b) << std::endl;
+		std::cout << "max = " << ::max(a, b) << std::endl;
+	}
+	return (0);
 }
--- CPP7/ex00/whatever.hpp
+++ CPP7/ex00/whatever.hpp
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Template.hpp                                       :+:      :+:    :+:   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amarzana <amarzana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 18:07:19 by amarzana          #+#    #+#             */
-/*   Updated: 2023/02/26 18:53:36 by amarzana         ###   ########.fr       */
+/*   Updated: 2023/02/27 12:19:11 by amarzana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TEMPLATE_HPP
-# define TEMPLATE_HPP
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
 
 // swap function template
 template <typename T>
@@ -26,14 +26,14 @@ void swap(T& a, T& b)
 template <typename T>
 T min(T a, T b)
 {
-	return (a < b) ? a : b;
+	return ((a < b) ? a : b);
 }
 
 // max function template
 template <typename T>
 T max(T a, T b)
 {
-	return (a > b) ? a : b;
+	return ((a > b) ? a : b);
 }
 
 #endif
